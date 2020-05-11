79858c6c13d9690921975c558b9d9cff0a45c5c0
JadTawbi <jad.tawbi2@gmail.com>
2020-05-11 19:34:01 +0200

    Started Powerup System

---
+++ Assets/Scripts/PartBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartBehaviour : MonoBehaviour
+{
+    private const float speed = 150.0f, floor_position = 815;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movePart();
+    }
+    private void movePart()
+    {
+        transform.position += -transform.up * speed * Time.deltaTime;
+        if(Mathf.Abs(transform.position.x)>= floor_position)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
