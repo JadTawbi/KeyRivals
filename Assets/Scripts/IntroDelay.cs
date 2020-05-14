a4abd5b927248b5bf83a6fda8064b06534cdfcec
leowognum <65113993+leowognum@users.noreply.github.com>
2020-05-14 12:18:24 +0200

    Lights (& Animations) and Delay
    Added Delay of Song and Applause Sound Effects. Also added lights and lights animations.

---
+++ Assets/Scripts/IntroDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDelay : MonoBehaviour
+{
+    public GameObject weapon;
+
+    public float seconds;
+  
+
+    void Start()
+    {
+        StartCoroutine("wait");
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(seconds);
+        weapon.SetActive(true);
+    }
+}
