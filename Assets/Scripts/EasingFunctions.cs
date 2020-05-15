80e434b650494f2520f9ee886fed9752d6ab317e
JadTawbi <jad.tawbi2@gmail.com>
2020-05-15 19:40:55 +0200

    tried to add easing function to score

---
+++ Assets/Scripts/EasingFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasingFunctions : MonoBehaviour
+{
+	public float easeNone(float t, float b, float c, float d)
+	{
+		return c * t / d + b;
+	}
+	public float easeIn(float t, float b, float c, float d)
+	{
+		return c * t / d + b;
+	}
+	public float easeOut(float t, float b, float c, float d)
+	{
+		return c * t / d + b;
+	}
+	public float easeInOut(float t, float b, float c, float d)
+	{
+		return c * t / d + b;
+	}
+}
--- Assets/Scripts/ScoreBehaviour.cs
+++ Assets/Scripts/ScoreBehaviour.cs
@@ -9,12 +9,22 @@ public class ScoreBehaviour : MonoBehaviour
     private int multiplier_amount, score_amount, streak_counter;
     private TextMeshPro multiplier_TMPro, score_TMPro;
 
+    //score easing
+    public EasingFunctions easing;
+    private float easing_timer, easing_timer_max;
+    private int amount_to_add, amount_before_easing;
+    private List<int> added_amounts;
+
     void Start()
     {
         score_amount = 0;
         multiplier_amount = 1;
         multiplier_TMPro = multiplier.GetComponent<TextMeshPro>();
         score_TMPro = score.GetComponent<TextMeshPro>();
+
+
+        //easing_timer = amount_to_add = amount_before_easing = added_amounts[0] = 0;
+        //easing_timer_max = 100.0f;
     }
 
     void Update()
@@ -24,12 +34,32 @@ public class ScoreBehaviour : MonoBehaviour
 
     private void displayText()
     {
+        //score_amount = (int)(easing.easeNone(easing_timer, (float) amount_before_easing, (float) added_amounts[0], easing_timer_max));
+
+        //if (easing_timer >= easing_timer_max)
+        //{
+        //    added_amounts.Remove(added_amounts[0]);
+        //    easing_timer = 0;
+        //    amount_before_easing = score_amount;
+        //}
+        //else
+        //{
+        //    easing_timer++;
+        //    if (easing_timer >= easing_timer_max)
+        //    {
+        //        easing_timer = easing_timer_max;
+        //    }
+        //}
+
         multiplier_TMPro.text = multiplier_amount.ToString();
         score_TMPro.text = score_amount.ToString();
     }
 
     public void addScore(int score_to_add)
     {
+        //amount_to_add = score_to_add * multiplier_amount;
+        //added_amounts.Add(amount_to_add);
+
         score_amount += score_to_add * multiplier_amount;
         streak_counter++;
         increaseMultiplier();
