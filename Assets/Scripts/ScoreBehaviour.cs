9abe621f0dc597e02907c3544542827a012e1a0b
JadTawbi <jad.tawbi2@gmail.com>
2020-05-28 22:34:54 +0200

    Easing scores on Win Screen and ingame

--- Assets/Scripts/ScoreBehaviour.cs
+++ Assets/Scripts/ScoreBehaviour.cs
@@ -9,13 +9,10 @@ public class ScoreBehaviour : MonoBehaviour
     private int multiplier_amount, streak_counter;
     [System.NonSerialized]
     public int score_amount;
+    private int score_displayed, score_before_adding;
+    private float t;
+    private const float EASING_TIME = 0.2f;
     private TextMeshPro multiplier_TMPro, score_TMPro;
-
-    //score easing
-    public EasingFunctions easing;
-    private float easing_timer, easing_timer_max;
-    private int amount_to_add, amount_before_easing;
-    private List<int> added_amounts;
 
     void Start()
     {
@@ -24,47 +21,35 @@ public class ScoreBehaviour : MonoBehaviour
         multiplier_TMPro = multiplier.GetComponent<TextMeshPro>();
         score_TMPro = score.GetComponent<TextMeshPro>();
 
-
-        //easing_timer = amount_to_add = amount_before_easing = added_amounts[0] = 0;
-        //easing_timer_max = 100.0f;
+        score_displayed = score_before_adding = 0;
+        t = 0.0f;
     }
 
     void Update()
     {
+        easeScore();
         displayText();
     }
 
     private void displayText()
     {
-        //score_amount = (int)(easing.easeNone(easing_timer, (float) amount_before_easing, (float) added_amounts[0], easing_timer_max));
-
-        //if (easing_timer >= easing_timer_max)
-        //{
-        //    added_amounts.Remove(added_amounts[0]);
-        //    easing_timer = 0;
-        //    amount_before_easing = score_amount;
-        //}
-        //else
-        //{
-        //    easing_timer++;
-        //    if (easing_timer >= easing_timer_max)
-        //    {
-        //        easing_timer = easing_timer_max;
-        //    }
-        //}
-
         multiplier_TMPro.text = multiplier_amount.ToString();
-        score_TMPro.text = score_amount.ToString();
+        score_TMPro.text = score_displayed.ToString();
     }
 
     public void addScore(int score_to_add)
     {
-        //amount_to_add = score_to_add * multiplier_amount;
-        //added_amounts.Add(amount_to_add);
-
+        score_before_adding = score_amount;
+        t = 0;
         score_amount += score_to_add * multiplier_amount;
         streak_counter++;
         increaseMultiplier();
+    }
+
+    public void easeScore()
+    {
+        t = Mathf.MoveTowards(t, 1.0f, Time.deltaTime / EASING_TIME);
+        score_displayed = (int)Mathf.Lerp(score_before_adding, score_amount, t);
     }
 
     private void increaseMultiplier()
--- Assets/Scripts/WinScreenBehaviour.cs
+++ Assets/Scripts/WinScreenBehaviour.cs
@@ -9,33 +9,51 @@ public class WinScreenBehaviour : MonoBehaviour
     [System.NonSerialized]
     public static int player1_score, player2_score;
     public TextMeshProUGUI player1_score_TMP, player2_score_TMP, winner_score_TMP, winner_TMP;
-    public GameObject player1, player2;
+    public GameObject player1, player2, winner;
+    private float t1, t2, player1_easing_time, player2_easing_time;
+    private const float SCORE_TO_TIME_FACTOR = 100000.0f;
+    private int player1_displayed_score, player2_displayed_score;
 
     // Start is called before the first frame update
     void Start()
     {
-        player1_score_TMP.text = player1_score.ToString();
-        player2_score_TMP.text = player2_score.ToString();
+        t1 = t2 = 0.0f;
+        player1_displayed_score = player2_displayed_score = 0;
+        //player1_score = 500000;
+        //player2_score = 250000;
+        player1_easing_time = player1_score / SCORE_TO_TIME_FACTOR;
+        player2_easing_time = player2_score / SCORE_TO_TIME_FACTOR;
         if(player1_score >= player2_score)
         {
-            winner_score_TMP.text = player1_score.ToString();
             winner_TMP.text = "Player 1";
-            player2.SetActive(true);
-            player1.SetActive(false);
+            winner_score_TMP.text = player1_score.ToString();
         }
         else
         {
-            winner_score_TMP.text = player2_score.ToString();
             winner_TMP.text = "Player 2";
-            player1.SetActive(true);
-            player2.SetActive(false);
+            winner_score_TMP.text = player2_score.ToString();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        easeScore();
+
+        player1_score_TMP.text = player1_displayed_score.ToString();
+        player2_score_TMP.text = player2_displayed_score.ToString();
+        if (t1 == 1.0f && t2 == 1.0f)
+        {
+            winner.SetActive(true);
+            if (player1_score >= player2_score)
+            {
+                player1.SetActive(false);
+            }
+            else
+            {
+                player2.SetActive(false);
+            }
+        }
     }
 
     public void quitToMainMenu()
@@ -44,5 +62,17 @@ public class WinScreenBehaviour : MonoBehaviour
         player1.SetActive(false);
         player2.SetActive(false);
         player1_score = player2_score = 0;
+    }
+
+    private void easeScore()
+    {
+        //if (player1_displayed_score >= player1_score - player1_score % 10)
+        //{
+
+        //}
+        t1 = Mathf.MoveTowards(t1, 1.0f, Time.deltaTime/player1_easing_time);
+        t2 = Mathf.MoveTowards(t2, 1.0f, Time.deltaTime/player2_easing_time);
+        player1_displayed_score = (int)Mathf.Lerp(0, player1_score, t1);
+        player2_displayed_score = (int)Mathf.Lerp(0, player2_score, t2);
     }
 }
