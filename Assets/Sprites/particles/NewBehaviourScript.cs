61e5f4991a6e9b0fe4bc270e1cf49615bb1fa5f4
Valentyn Lyevyentsov <valentyn-l@users.noreply.github.com>
2020-05-30 22:36:09 +0200

    Particle FX 0.2
    Fixed menu particle effects. Adjusted sprite rendering to higher res.

---
+++ Assets/Sprites/particles/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public ParticleSystem P1;
+    public ParticleSystem P2;
+    public ParticleSystem P3;
+
+
+    void ActivateFirst()
+    {
+        P1.Play();
+    }
+
+    void ActivateSecond()
+    {
+        P2.Play();
+    }
+
+    void ActivateThird()
+    {
+        P3.Play();
+    }
+
+    void DeactivateBoth()
+    {
+        P1.Stop();
+        P2.Stop();
+        P3.Stop();
+    }
+}
