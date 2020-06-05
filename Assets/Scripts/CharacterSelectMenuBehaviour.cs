ce411b83a1098a72311affe166efa1874f6d848f
Alfakyn <60134035+Alfakyn@users.noreply.github.com>
2020-06-05 16:29:49 +0200

    Character selection screen re-worked
    - Added randomize button
    - Rearranged character sprites
    - Added a display for which character is currently selected

--- Assets/Scripts/CharacterSelectMenuBehaviour.cs
+++ Assets/Scripts/CharacterSelectMenuBehaviour.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelectMenuBehaviour : MonoBehaviour
 {
     public static PlayerBehaviour.PlayerCharacter player1_character, player2_character;
+    public Button[] player1_buttons, player2_buttons;
+
+    private void OnEnable()
+    {
+        updateButtonColourPlayer1();
+        updateButtonColourPlayer2();
+    }
 
     public static void randomizeCharacters()
     {
         player1_character = (PlayerBehaviour.PlayerCharacter)Random.Range(0, 7);
         player2_character = (PlayerBehaviour.PlayerCharacter)Random.Range(0, 7);
     }
+    public void randomizePlayer1Character()
+    {
+        player1_character = (PlayerBehaviour.PlayerCharacter)Random.Range(0, 7);
+    }
+    public void randomizePlayer2Character()
+    {
+        player2_character = (PlayerBehaviour.PlayerCharacter)Random.Range(0, 7);
+    }
     public void loadBassfisherPlayer1()
     {
-        player1_character = PlayerBehaviour.PlayerCharacter.BassFisher; 
+        player1_character = PlayerBehaviour.PlayerCharacter.BassFisher;
     }    
     public void loadBassfisherPlayer2()
     {
@@ -68,4 +86,43 @@ public class CharacterSelectMenuBehaviour : MonoBehaviour
         player2_character = PlayerBehaviour.PlayerCharacter.Powerdog;
     }
 
+    public void updateButtonColourPlayer1()
+    {
+        ColorBlock color_block;
+        for (int i = 0; i < player1_buttons.Count(); i++)
+        {
+            color_block = player1_buttons[i].colors;
+
+            if (i == (int)player1_character)
+            {
+                color_block.normalColor = WeaponBehaviour.blue;
+            }
+            else
+            {
+                color_block.normalColor = Color.white;
+            }
+
+            player1_buttons[i].colors = color_block;
+        }
+    }
+
+    public void updateButtonColourPlayer2()
+    {
+        ColorBlock color_block;
+        for (int i = 0; i < player2_buttons.Count(); i++)
+        {
+            color_block = player2_buttons[i].colors;
+
+            if (i == (int)player2_character)
+            {
+                color_block.normalColor = WeaponBehaviour.red;
+            }
+            else
+            {
+                color_block.normalColor = Color.white;
+            }
+
+            player2_buttons[i].colors = color_block;
+        }
+    }
 }
